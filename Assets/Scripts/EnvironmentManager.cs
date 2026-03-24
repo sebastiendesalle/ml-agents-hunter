2a45d23d314cf07cc57d72e1c75422409a06b8c1
sebastien <sebastien.desalle@student.ap.be>
2026-03-24 15:10:12 +0100

    Add environment and raycasting

---
+++ Assets/Scripts/EnvironmentManager.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+public class EnvironmentManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
